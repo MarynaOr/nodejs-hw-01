9f546ff465bdf51082b0b501fa2d0649b4142b6c
Maryna <89296066105marina@gmal.com>
2025-05-30 12:37:30 +0200

    task-2

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,9 @@
 export const getAllContacts = async () => {};
 
 console.log(await getAllContacts());
+console.log('say hello getAllContacts');
+
+
+
+
+// node src/scripts/getAllContacts
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { promises as fs } from 'fs';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = JSON.stringify(updatedContacts, null, 2);
+    await fs.writeFile(PATH_DB, data, 'utf-8');
+    return ;
+  } catch (error) {
+    console.error('writeContacts', error.message);
+    throw error;
+  }
+};
+
+export default writeContacts;
