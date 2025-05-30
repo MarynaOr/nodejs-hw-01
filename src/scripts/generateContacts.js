2df47094b836d9bc69d0eff72cff51d8bc70cc40
Maryna <89296066105marina@gmal.com>
2025-05-30 16:49:37 +0200

    task-3

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,28 @@
-const generateContacts = async (number) => {};
 
-generateContacts(5);
+import { createFakeContact } from '../utils/createFakeContact.js';
+import readContacts from '../utils/readContacts.js';
+import writeContacts from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+
+
+  const contacts = await readContacts();
+
+
+  for (let i = 0; i < number; i++) {
+    const contact = createFakeContact();
+    contacts.push(contact);
+  }
+
+  try {
+    await writeContacts(contacts);
+  } catch (error) {
+    console.error('generateContacts', error.message);
+  }
+
+    return contacts;
+};
+
+generateContacts(1);
+
+// node src/scripts/generateContacts.js
